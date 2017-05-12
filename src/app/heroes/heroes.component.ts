9df367ef2e2ddbdc4fb58b1c474c15c2371a1ed6
Carlos Rojas <ing.carlosandresrojas@gmail.com>
2017-05-12 12:00:46 -0500

    routing stuff

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import 'rxjs/add/operator/switchMap';
+import { Component, OnInit }      from '@angular/core';
+import { Router } from '@angular/router';
+
+import { Hero } from '../hero'; 
+import { HeroServiceService } from '../hero-service.service';
 
 @Component({
   selector: 'app-heroes',
@@ -6,11 +11,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  heroes: Hero[];
+  selectedHero: Hero;
 
+  constructor(
+    private router: Router,
+    private heroService: HeroServiceService) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
 
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
 }
